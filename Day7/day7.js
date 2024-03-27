c9f644105da3047f8fb281850b36ef3498aeb9ea
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2024-03-27 20:58:03 +0700

    new commit

--- Day7/day7.js
+++ Day7/day7.js
@@ -55,6 +55,7 @@ function createListProduct(product) {
   }
 
   // Thêm vào HTML
+  li.id = `product_id_${product.id}`;
   li.innerHTML = `
     ID: <b class="productID">${product.id}</b><br />
     Tên sản phẩm:
@@ -75,44 +76,77 @@ for (let i = 0; i < list_product.length; i++) {
 }
 
 button[0].addEventListener("click", function () {
-  for (let i = 1; i <= list_product.length; i++) {
-    if (input.value == i) {
-      let take_li = document.querySelector(`li:nth-child(${input.value})`);
+  let li_in_ul = ul.getElementsByTagName("li");
+  // Tạo ta 1 mảng và bên trong mảng này sẽ chứa toàn bộ ID đang có trên các thẻ li trong ul
+  let emptyArray = [];
+
+  for (let i = 0; i < li_in_ul.length; i++) {
+    let li_id = li_in_ul[i].getAttribute("id");
+    emptyArray.push(li_id.substring(11, li_id.length));
+  }
+
+  console.log(emptyArray);
+
+  for (let i = 0; i < emptyArray.length; i++) {
+    if (input.value == emptyArray[i]) {
+      let take_li = document.getElementById(`product_id_${emptyArray[i]}`);
       take_li.style.backgroundColor = "red";
     } else {
-      let take_li = document.querySelector(`li:nth-child(${i})`);
+      let take_li = document.getElementById(`product_id_${emptyArray[i]}`);
       take_li.style.backgroundColor = "white";
     }
   }
 });
 
+// Render with category
+function renderWithCategory(category) {
+  ul.innerHTML = "";
+  for (let z = 0; z < list_product.length; z++) {
+    if (list_product[z].category == category) {
+      createListProduct(list_product[z]);
+    }
+  }
+}
+
 for (let i = 1; i < button.length; i++) {
   button[i].addEventListener("click", function () {
-    // Shirt
-    if (i == 1) {
-      for (let z = 0; z < list_product.length; z++) {
-        if (list_product[z].category == "shirt") {
-          createListProduct(list_product[z]);
-        }
-      }
-    }
+    renderWithCategory(button[i].innerText.toLowerCase());
 
-    //Shoes
-    else if (i == 2) {
-      for (let z = 0; z < list_product.length; z++) {
-        if (list_product[z].category == "shoes") {
-          createListProduct(list_product[z]);
-        }
-      }
-    }
+    button[i].style.backgroundColor = "red";
 
-    // Bag
-    else if (i == 3) {
-      for (let z = 0; z < list_product.length; z++) {
-        if (list_product[z].category == "bag") {
-          createListProduct(list_product[z]);
-        }
+    for (let z = 0; z < button.length; z++) {
+      if (i != z) {
+        button[z].style.backgroundColor = "";
       }
     }
+    // // Shirt
+    // if (i == 1) {
+    //   ul.innerHTML = "";
+    //   for (let z = 0; z < list_product.length; z++) {
+    //     if (list_product[z].category == "shirt") {
+    //       createListProduct(list_product[z]);
+    //     }
+    //   }
+    // }
+
+    // //Shoes
+    // else if (i == 2) {
+    //   ul.innerHTML = "";
+    //   for (let z = 0; z < list_product.length; z++) {
+    //     if (list_product[z].category == "shoes") {
+    //       createListProduct(list_product[z]);
+    //     }
+    //   }
+    // }
+
+    // // Bag
+    // else if (i == 3) {
+    //   ul.innerHTML = "";
+    //   for (let z = 0; z < list_product.length; z++) {
+    //     if (list_product[z].category == "bag") {
+    //       createListProduct(list_product[z]);
+    //     }
+    //   }
+    // }
   });
 }
